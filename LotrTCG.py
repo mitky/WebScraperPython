d6e488d0eadfa6b3a0218c78bee2414ac3229e45
arabovs <arabov.simeon1990@gmail.com>
2022-09-09 21:07:07 +0100

    changes!

--- LotrTCG.py
+++ LotrTCG.py
@@ -1,28 +1,45 @@
-#scrape the Lotr site
-
+from requests import request
 import requests
+from gql import Client, gql
 from bs4 import BeautifulSoup
-
+from gql.transport.requests import RequestsHTTPTransport
 URL = "https://lotrtcgwiki.com/wiki/grand" # site url
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, "html.parser")
 
-cards_table = soup.find_all('table', class_='inline') # look for class in div 
+HASURA_URL = "https://lotrwebscrapper.herokuapp.com/v1/graphql"
+transport = RequestsHTTPTransport(
+    url=HASURA_URL,
+    verify=True,
+    retries=3,
+)
 
+
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+def runGQL(name, edition):
+
+    query = gql("""mutation MyMutation($name: String!, $edition: String!) {
+      insert_lotr_all_cards(objects: {name: $name, edition: $edition}) {
+        affected_rows
+      }
+    }""")
+
+    params = {
+        "name": name,
+        "edition": edition
+    }
+    result = client.execute(query, variable_values=params)
+
+
+cards_table = soup.find_all('table', class_='inline') # look for class in div 
 
 #look for specific tags
 for cards in cards_table:
     rows = cards.find_all('tr')
-    #print(rows)
     for row in rows:
         card_id = row.find('td')
-        card_name = row.find('td', class_= 'col1').a
-        card_type = row.find('td', class_= 'col2').a
-        print(card_id.text)
-        print(card_name)
-        print(card_type)
-        
-    
-    
+        card_name = row.find('td', class_= 'col1').string
+        card_type = row.find('td', class_= 'col2').string
+        runGQL(str(card_name),str(card_name))
     
